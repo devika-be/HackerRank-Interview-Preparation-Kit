853c8ca2b4784d89cd7446db7f620846abc92caf
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-10-14 09:39:04 +0530

    Create Arrays-2D Array - DS.py

---
+++ Arrays-2D Array - DS.py
@@ -0,0 +1,39 @@
+#Problem Link : https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+#Ans:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    li = []
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            li.append(sum(arr[i][j:j+3]+arr[i+2][j:j+3])+arr[i+1][j+1])
+    return max(li)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
