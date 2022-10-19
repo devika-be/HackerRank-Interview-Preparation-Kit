ce901c876a7ed52ba04e35cb9ca6606d5bb74a3d
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-10-19 21:33:35 +0530

    Create Dictionaries and Hashmaps-Hash Tables: Ransom Note.py

---
+++ Dictionaries and Hashmaps-Hash Tables: Ransom Note.py
@@ -0,0 +1,35 @@
+#Problem Link : https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+#Ans:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    # Write your code here
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    n = int(first_multiple_input[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
