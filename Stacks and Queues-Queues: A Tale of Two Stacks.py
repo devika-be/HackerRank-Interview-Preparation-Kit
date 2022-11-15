87ced704149442337124571b98915b07891d4895
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-11-15 20:43:33 +0530

    Create Stacks and Queues-Queues: A Tale of Two Stacks.py

---
+++ Stacks and Queues-Queues: A Tale of Two Stacks.py
@@ -0,0 +1,38 @@
+#Problem Link : https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+
+#Ans:
+
+class MyQueue(object):
+    def __init__(self):
+        self.store = []
+        self.delivery = []
+
+    def peek(self):
+        if not self.delivery:
+            while self.store:
+                self.delivery.append(self.store.pop())
+
+        return self.delivery[-1]
+
+    def pop(self):
+        if not self.delivery:
+            while self.store:
+                self.delivery.append(self.store.pop())
+
+        return self.delivery.pop()
+
+    def put(self, value):
+        self.store.append(value)
+
+
+queue = MyQueue()
+t = int(input())
+for line in range(t):
+    values = map(int, input().split())
+    values = list(values)
+    if values[0] == 1:
+        queue.put(values[1])
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print(queue.peek())
